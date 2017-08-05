eefaa259188b06a4ab76ecc5fc0e14e838a1761f
mafuyuk <mafuyuk.m@gmail.com>
2017-08-05 17:08:48 +0900

    do not use electron rapper

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const electron = require('electron');
-const menubar = require('menubar');
-const path = require('path');
+const { app, Tray, Menu } = require('electron');
 
-const mb = menubar({
-  dir: path.join(__dirname, '/app'),
-  width: 200,
-  height: 200,
-  icon: path.join(__dirname, '/app/icon.png'),
-  preloadWindow: true,
-  alwaysOnTop: false
-});
+let appIcon = null;
 
-mb.on('ready', function ready() {
-  console.log('app is ready');
+app.on('ready', function ready() {
+  appIcon = new Tray(`${__dirname}/app/icon.png`);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Item1', type: 'radio'},
+    {label: 'Item2', type: 'radio'},
+    {label: 'Item3', type: 'radio', checked: true},
+    {label: 'Item4', type: 'radio'}
+  ]);
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
+
+  appIcon.setImage(`${__dirname}/app/icon2.png`);
+  appIcon.setImage(`${__dirname}/app/icon1.png`);
 });
