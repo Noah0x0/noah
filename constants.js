8e3bddecce2930f863142ae34398100a7ac38ad8
usagi.f@gmail.com <usagi.f@gmail.com>
2017-08-09 21:14:00 +0900

    update: separate lib method and constants

---
+++ constants.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    // icon path
+    trayIcon1: `${ __dirname }/icon/tray-icon1.png`,
+    trayIcon2: `${ __dirname }/icon/tray-icon2.png`,
+    trayIcon3: `${ __dirname }/icon/tray-icon3.png`,
+    trayIcon4: `${ __dirname }/icon/tray-icon4.png`,
+    trayIcon5: `${ __dirname }/icon/tray-icon5.png`,
+    pushIcon: `${ __dirname }/icon/push-icon.png`,
+
+    // API URL
+    waterLevelURL: '',
+    rainFallURL: '',
+
+    // HTML path
+    windowURL: `file://${__dirname}/app/index.html`
+};
