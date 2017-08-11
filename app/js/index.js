67b773825b7045178cceb3e5f2da01b5b4699d98
usagi-f <usagi.f@gmail.com>
2017-08-11 16:56:19 +0900

    add: context menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,27 @@
 'use strict';
 
-const ipcRenderer = require('electron').ipcRenderer;
+const { ipcRenderer, remote } = require('electron');
+const Menu = remote.Menu;
+const MenuItem = remote.MenuItem;
 
 ipcRenderer.on('dataReflect', (ev, data) => {
     const testDiv = document.getElementById('test');
     testDiv.innerHTML = data;
+});
+
+const iconCog = document.querySelector('.fa-cog');
+iconCog.addEventListener('click', () => {
+    const menu = new Menu();
+    menu.append(new MenuItem({
+        label: 'About noah',
+        type: 'normal',
+        role: 'about'
+    }));
+    menu.append(new MenuItem({
+        label: 'Quit noah',
+        type: 'normal',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit'
+    }));
+    menu.popup();
 });
