25b10a822849f2ab0285dcb4f19f5d414f688c9f
mafuyuk <mafuyuk.m@gmail.com>
2017-08-14 15:30:24 +0900

    add: create QR code

--- app/js/components/qrcode.js
+++ app/js/components/qrcode.js
@@ -4,9 +4,10 @@ import React from 'react';
 
 const QRCode = (props) => {
   const { getMeridianSearchURL } = props;
+  const qrcode = `http://chart.apis.google.com/chart?chs=150x150&cht=qr&chl=${ getMeridianSearchURL('新神田小学校') }`;
   return (
     <div className="noah-qrcode">
-      <p> {getMeridianSearchURL('新神田小学校')} </p>
+      <img src={qrcode} alt="QRコード" />
     </div>
   );
 };
