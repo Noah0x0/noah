f13c5cfd87a53208ab3c2dc26f5ee469f4bb66f1
mafuyuk <mafuyuk.m@gmail.com>
2017-08-15 14:09:20 +0900

    fix object

--- app/js/app.js
+++ app/js/app.js
@@ -50,7 +50,10 @@ class App extends Component {
 
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(
-        (position) => resolve([position.coords.latitude, position.coords.longitude]),
+        (position) => resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }),
         (err) => reject('ERROR(' + err.code + '): ' + err.message),
         options,
       );
