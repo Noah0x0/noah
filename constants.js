ad6bbf191d2b614c429c06f75b8c22de69962650
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-19 01:48:43 +0900

    update: New Icons.

--- constants.js
+++ constants.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     // icon path
-    trayIcon1: `${ __dirname }/icon/tray/mac-taskbarIconL1_mac.png`,
-    trayIcon2: `${ __dirname }/icon/tray/mac-taskbarIconL2_mac.png`,
-    trayIcon3: `${ __dirname }/icon/tray/mac-taskbarIconL3_mac.png`,
-    trayIcon4: `${ __dirname }/icon/tray/mac-taskbarIconL4_mac.png`,
-    trayIcon5: `${ __dirname }/icon/tray/mac-taskbarIconL5_mac.png`,
-    pushIcon: `${ __dirname }/icon/push-icon.png`,
-    aboutIcon: `${ __dirname }/icon/appIcon.png`,
+    trayIcon1: `${ __dirname }/icon/tray/tray-icon1_mac@3x.png`,
+    trayIcon2: `${ __dirname }/icon/tray/tray-icon2_mac@3x.png`,
+    trayIcon3: `${ __dirname }/icon/tray/tray-icon3_mac@3x.png`,
+    trayIcon4: `${ __dirname }/icon/tray/tray-icon4_mac@3x.png`,
+    trayIcon5: `${ __dirname }/icon/tray/tray-icon5_mac@3x.png`,
+    pushIcon: `${ __dirname }/icon/push-icon@2x.png`,
+    aboutIcon: `${ __dirname }/icon/appIcon_mac@1x.png`,
 
     // HTML path
     windowURL: `file://${__dirname}/app/index.html`,
